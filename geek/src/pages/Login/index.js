95b4bb7edeb83002c732c5aecf456a2d4b9b4d49
Pinger <947384988@qq.com>
2022-01-26 19:34:54 +0800

    登入功能-基本结构

--- geek/src/pages/Login/index.js
+++ geek/src/pages/Login/index.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
-import { Button, DatePicker } from 'antd'
+import { Card } from 'antd'
+import './index.css'
+import logo from '../../assets/logo.png'
 export default class Login extends Component {
   render() {
     return (
-      <div>
-        登入组件
-        <Button type="primary">登入</Button><br />
-        <DatePicker />
+      <div className="login">
+        <Card className="login-container">
+          {/* 正常情况下 使用<img src="" alt="" />导入图片，
+          但是src=""是字符串不能被webpack处理，这个图片地址必须是一个动态的*/}
+          <img className="login-logo" src={logo} alt="" />
+        </Card>
       </div>
     )
   }
