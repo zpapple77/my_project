e87d6cf50d92104d7c6306b5fec6c56587225c0a
Pinger <947384988@qq.com>
2022-01-26 19:04:20 +0800

    使用antd组件库

--- geek/src/index.js
+++ geek/src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//导入antd全局样式
+import 'antd/dist/antd.css'
+//自己的全局样式
 import './index.css';
 import App from './App';
 
--- geek/src/pages/Login/index.js
+++ geek/src/pages/Login/index.js
@@ -1,7 +1,13 @@
-import React, { Component } from 'react';
-
+import React, { Component } from 'react'
+import { Button, DatePicker } from 'antd'
 export default class Login extends Component {
   render() {
-    return <div>登入组件</div>;
+    return (
+      <div>
+        登入组件
+        <Button type="primary">登入</Button><br />
+        <DatePicker />
+      </div>
+    )
   }
 }
